925617bfe8de7195856d8f95825e29b915b92343
Choda Subramanyam <149325423+subramanyamchoda@users.noreply.github.com>
2025-09-11 23:50:05 +0530

    Update urls.py

--- HealthAssist/HealthAssist/urls.py
+++ HealthAssist/HealthAssist/urls.py
@@ -2,8 +2,14 @@
 from django.contrib import admin
 from django.urls import path,include
 
+
+def home(request):
+    return JsonResponse({"status": "ok", "message": "Backend is running"})
+    
 urlpatterns = [
     path('admin/', admin.site.urls),
+      path('', home), 
     path('user/',include('health.urls')),
     
 ]
+
